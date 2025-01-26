f6b3542fd146cfc1e96e1a67b524e537e2f48f6f
Abilov599 <abilovv599@gmail.com>
2025-01-26 20:45:37 +0400

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-import { Map } from "@/components/map-container/map";
+import { InteractiveMap } from "@/components/interactive-map";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-100 p-8">
-      <div className="max-w-6xl mx-auto">
-        <h1 className="text-3xl font-bold text-gray-900 mb-6">
-          Interactive SVG Map
-        </h1>
-        <div className="bg-white p-6 rounded-xl shadow-lg">
-          <Map />
-        </div>
-      </div>
-    </div>
-  );
+  return <InteractiveMap svgWidth={100} svgHeight={100}>
+         <rect x="0" y="0" width="100" height="100" fill="#f3f3f3" />
+      <rect x="10" y="10" width="30" height="30" fill="lightblue" />
+      <circle cx="50" cy="50" r="20" fill="orange" />
+      <text x="20" y="90" fontSize="10" fill="black">
+        Zoom and Pan me!
+      </text>
+  </InteractiveMap>
 }
