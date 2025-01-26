b76d62989ad449b2aa2db3e320d3648d6dee2537
Abilov599 <abilovv599@gmail.com>
2025-01-26 23:51:23 +0400

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import { InteractiveMap } from "@/components/interactive-map";
 export default function Home() {
   return (
     <div style={{ width: "100%", height: "calc(100vh)", overflow: "hidden" }}>
-      <InteractiveMap imageSrc="Alexander_III_conquest_from_Issos_to_Babylon-fr.svg" />
+      {/* <InteractiveMap imageSrc="Alexander_III_conquest_from_Issos_to_Babylon-fr.svg" /> */}
+      <InteractiveMap imageSrc="map-absheron.svg" />
     </div>
   );
 }
