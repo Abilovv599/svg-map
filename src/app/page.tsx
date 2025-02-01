978f1bc9b2ddab7ca53fa0ebab376fa96386c314
Abilov599 <abilovv599@gmail.com>
2025-02-01 16:42:36 +0400

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import { Suspense } from "react";
+
 import { InteractiveMap } from "@/components/interactive-map";
 import Map from "../assets/map-absheron.svg";
 
 export default function Home() {
   return (
     <div style={{ width: "100%", height: "calc(100vh)", overflow: "hidden" }}>
-      <InteractiveMap>
-        <Map />
-      </InteractiveMap>
+      <Suspense fallback={<div>Loading...</div>}>
+        <InteractiveMap>
+          <Map />
+        </InteractiveMap>
+      </Suspense>
     </div>
   );
 }
